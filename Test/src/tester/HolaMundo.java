319b3fa76de8f878067844f0ba45cb01daa4a251
Administrador <Administrador@WIN-2441FMASZ8L>
2017-01-16 17:39:14 +0100

    2º) eliminada clase testers y modificado HolaMundo

--- Test/src/tester/HolaMundo.java
+++ Test/src/tester/HolaMundo.java
@@ -3,7 +3,7 @@ package tester;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola");
+		System.out.println("Hola Mundo");
 		// TODO Auto-generated method stub
 
 	}
